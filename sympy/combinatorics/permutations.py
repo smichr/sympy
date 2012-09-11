0ff03a72f09a0649e11643c81848cfa4ee821a43
Chris Smith <smichr@gmail.com>
2012-09-11 12:13:58 +0545

    permutations: add __rtruediv__ for Python 3

--- sympy/combinatorics/permutations.py
+++ sympy/combinatorics/permutations.py
@@ -1067,8 +1067,11 @@ class Permutation(Basic):
             raise ValueError(
             'only a permutation or 1 may be divided by a permutation')
 
-    def __truediv__(self, other): # Python 3
+    def __rtruediv__(self, other): # Python 3
         return other.__rdiv__(self)
+
+    def __truediv__(self, other): # Python 3
+        return other.__rtruediv__(self)
 
     def __div__(self, other): # Python 2
         return other.__rdiv__(self)
