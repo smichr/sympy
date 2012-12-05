da8c428579bda407e411aaa845f92aba25be3e29
Chris Smith <smichr@gmail.com>
2012-12-05 08:32:57 +0545

    3539b: do not let Mul become more complex

--- sympy/core/tests/test_match.py
+++ sympy/core/tests/test_match.py
@@ -614,6 +614,8 @@ def test_issue_3539():
     a = Wild('a')
     x = Symbol('x')
     assert (x - 2).match(a - x) is None
+    assert (6/x).match(a*x) is None
+    assert (6/x**2).match(a/x) == {a: 6/x}
 
 
 def test_default_exclusion():
