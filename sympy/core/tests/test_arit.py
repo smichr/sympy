e026a1fd4feb63ffbbb79341c5ba22f5b6d6ef9e
Sergey B Kirpichev <skirpichev@gmail.com>
2014-08-28 14:18:00 +0400

    Small improvments for Mul._eval_is_real/imaginary
    1. removed fuzzy_not import (it's redundant)
    2. slight formatting changes
    3. add an early exit if t is not a complex number
    4. renamed: is_zero -> zero (like the real variable)
    5. simplify "if real and is_zero is False" branch of _eval_is_real

--- sympy/core/tests/test_arit.py
+++ sympy/core/tests/test_arit.py
@@ -492,7 +492,7 @@ def test_Add_is_even_odd():
 
 def test_Mul_is_negative_positive():
     x = Symbol('x', real=True)
-    y = Symbol('y', real=False)
+    y = Symbol('y', real=False, complex=True)
 
     neg = Symbol('neg', negative=True)
     pos = Symbol('pos', positive=True)
@@ -1147,7 +1147,7 @@ def test_Mul_is_imaginary_real():
     assert (r*i*ii).is_real is True
 
     # Github's issue 5874:
-    nr = Symbol('nr', real=False)
+    nr = Symbol('nr', real=False, complex=True)
     a = Symbol('a', real=True, nonzero=True)
     b = Symbol('b', real=True)
     assert (i*nr).is_real is None
@@ -1158,7 +1158,7 @@ def test_Mul_is_imaginary_real():
     a = Symbol('a', real=True, zero=False)
     b = Symbol('b', real=True)
     bb = Symbol('b', real=True, zero=False)
-    c = Symbol('c', real=False)
+    c = Symbol('c', real=False, complex=True)
     e1 = Mul(a, b, c, evaluate=False)
     e2 = Mul(b, a, c, evaluate=False)
     assert e1.is_imaginary is None
