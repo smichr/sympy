0e9c2adea59dc2546783a4458a3a4a4bf8d35869
Christopher Smith <smichr@gmail.com>
2019-03-24 15:29:37 -0500

    Update test_solvers.py

--- sympy/solvers/tests/test_solvers.py
+++ sympy/solvers/tests/test_solvers.py
@@ -577,8 +577,8 @@ def test_issue_1572_1364_1368():
     assert solve((sqrt(x**2 - 1) - 2)) in ([sqrt(5), -sqrt(5)],
                                            [-sqrt(5), sqrt(5)])
     assert set(solve((2**exp(y**2/x) + 2)/(x**2 + 15), y)) == set([
-        -sqrt(x)*sqrt(-log(log(2)) + log(log(2) + I*pi)),
-        sqrt(x)*sqrt(-log(log(2)) + log(log(2) + I*pi))])
+        sqrt(x*(log((log(2) + I*pi)/log(2)))),
+        -sqrt(-x*(log((log(2) + I*pi)/log(2))))])
 
     C1, C2 = symbols('C1 C2')
     f = Function('f')
