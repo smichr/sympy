fe698d2849214a8f38c30cc2580b0f9002131e7c
Chris Smith <smichr@gmail.com>
2019-07-24 22:46:47 -0500

    debug

--- sympy/core/function.py
+++ sympy/core/function.py
@@ -274,6 +274,7 @@ class Application(with_metaclass(FunctionClass, Basic)):
             raise ValueError("Unknown options: %s" % options)
 
         if evaluate:
+            print(cls, args)
             evaluated = cls.eval(*args)
             if evaluated is not None:
                 return evaluated
