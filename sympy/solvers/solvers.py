9fd15b41347392cabbea40dfa7314296003d4377
Christopher Smith <smichr@gmail.com>
2021-10-08 14:06:47 -0500

    Update solvers.py

--- sympy/solvers/solvers.py
+++ sympy/solvers/solvers.py
@@ -656,7 +656,6 @@ def solve(f, *symbols, **flags):
     (which can be quite long). If the expression is univariate, ``CRootOf``
     instances will be returned instead:
 
-        >>> from sympy import count_ops
         >>> solve(x**3 - x + 1)
         [-(-1/2 + sqrt(3)*I/2)*(3*sqrt(69)/2 + 27/2)**(1/3)/3 + 1/((1/2 -
         sqrt(3)*I/2)*(3*sqrt(69)/2 + 27/2)**(1/3)), 1/((1/2 +
