ef55c87cadf5aac3cc36f2e0706aee39f6c603db
Chris Smith <smichr@gmail.com>
2021-10-08 10:40:44 -0500

    sympy/core/basic.py

--- sympy/core/basic.py
+++ sympy/core/basic.py
@@ -1880,6 +1880,9 @@ class Basic(Printable, metaclass=ManagedProperties):
             # call the freshly monkey-patched method
             return self._sage_()
 
+    def could_extract_minus_sign(self):
+        return False
+
 
 class Atom(Basic):
     """
