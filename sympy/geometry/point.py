4327d097a46af6e6520788bfc7dd03f27bb5f9f5
Chris Smith <smichr@gmail.com>
2024-03-17 13:17:38 -0500

    use quick return in is_scalar_multiple

--- sympy/geometry/point.py
+++ sympy/geometry/point.py
@@ -633,11 +633,12 @@ class Point(GeometryEntity):
         # 2d points happen a lot, so optimize this function call
         if s.ambient_dimension == 2:
             (x1, y1), (x2, y2) = s.args, o.args
-            rv = (x1*y2 - x2*y1).equals(0)
+            rv = (x1*y2 - x2*y1).equals(0)  # x1/x2 = y1/y2
             if rv is None:
                 raise Undecidable(filldedent(
                     '''Cannot determine if %s is a scalar multiple of
                     %s''' % (s, o)))
+            return rv
 
         # if the vectors p1 and p2 are linearly dependent, then they must
         # be scalar multiples of each other
